ad809bcea81f3bfef902485e14e516a613a073b0
Shinta Koyanagi <yancya@upec.jp>
2026-01-13 01:15:54 +0900

    Update formula to v1.0.0
    - Update URL to v1.0.0 tarball
    - Update SHA256 hash to match new release

--- Formula/reminder-cli.rb
+++ Formula/reminder-cli.rb
@@ -1,8 +1,8 @@
 class ReminderCli < Formula
   desc "CLI tool to manage iCloud Reminders"
   homepage "https://github.com/yancya/reminder-cli"
-  url "https://github.com/yancya/reminder-cli/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "efd1a889e7414e2c6b6ca44d04ba2cf704890a847297a8697113afcce23fa267"
+  url "https://github.com/yancya/reminder-cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "680d48d58c6e9fbce1d7aa5e885fd733331d3470fbe629eb6a83f2739ba3a4d8"
   license "WTFPL"
 
   depends_on :macos
