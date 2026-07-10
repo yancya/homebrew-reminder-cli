ab392acec3112e282a62a86be8b216d66f22aa25
Shinta Koyanagi <yancya@upec.jp>
2026-07-10 21:58:36 +0900

    test: 意図的に sha256 を壊して CI の fail を検証

--- Formula/reminder-cli.rb
+++ Formula/reminder-cli.rb
@@ -2,7 +2,7 @@ class ReminderCli < Formula
   desc "CLI tool to manage iCloud Reminders"
   homepage "https://github.com/yancya/reminder-cli"
   url "https://github.com/yancya/reminder-cli/releases/download/v1.0.1/reminder-cli-macos.tar.gz"
-  sha256 "4acc4757e4f1610faafa4025aac9250e9d69e835ed33e8b27b5f160224bfe44e"
+  sha256 "4acc4757e4f1610faafa4025aac9250e9d69e835ed33e8b27b5f160224bfe4f"
   license "WTFPL"
 
   depends_on arch: :arm64
